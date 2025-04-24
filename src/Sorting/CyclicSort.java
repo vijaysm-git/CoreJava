d67fb760ee1a60b83131312c567ca49680409d69
vijaysm-git <reachvijaysm@outlook.com>
2025-04-24 20:31:16 +0530

    sort

--- src/Sorting/CyclicSort.java
+++ src/Sorting/CyclicSort.java
@@ -1,28 +1,32 @@
-package BinarySearch;
+package Sorting;
 
 import java.util.Arrays;
+
+import static Sorting.SortingAlgos.swap;
 
 public class CyclicSort {
     public static void main(String[] args) {
         int[]arr={4,3,2,1,5};
-        cyclicSort(arr);
+        sort(arr);
 
             System.out.print(Arrays.toString(arr));
 
     }
-    static void cyclicSort(int[] arr){
+    static void sort(int[] arr){
         int i = 0;
         while(i<arr.length){
             int correctIndex = arr[i]-1;
-            if(arr[i]!= correctIndex) {
-                int temp = arr[i];
-                arr[i] = arr[temp];
-                arr[temp] = temp;
+            if(arr[i]!= arr[correctIndex]) {
+               swap(arr,i,correctIndex);
             }else{
                 i++;
             }
-
-
         }
+    }
+
+    static void swap(int[] arr, int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
--- src/Sorting/SortingAlgos.java
+++ src/Sorting/SortingAlgos.java
@@ -4,10 +4,22 @@ import java.util.Arrays;
 
 public class SortingAlgos {
     public static void main(String[] args) {
-        int[] arr = {1,-1,0,55,44};
-        selection(arr);
+        int[] arr = {5,3,4,1,2};
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
 
+    }
+    static void insertion(int[]arr){
+         for (int i = 0; i < arr.length -1; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
     }
 
     static void selection(int[]arr){
